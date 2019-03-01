6d25017253ee63228ca0df16aae4fc6baf41af5b
wenwei.qian <wenwei.qian@kangcenet.com>
2019-03-01 14:39:30 +0800

    修改base_url

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['base_url'] ='http://localhost/' ;
+$config['base_url'] ='https://www.qianwenwei.com/' ;
 $config['index_page'] = '?';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '.html';
